4f16d0ec253c108d3a3b65d3138be32dfcaccd5b
Andrew Meleka <andrew-nageh@hotmail.com>
2025-05-24 02:26:45 +0300

    fix sorted problem

--- currency/currency.go
+++ currency/currency.go
@@ -3,29 +3,41 @@ package currency
 import (
 	"fmt"
 
+	"github.com/AndrewMeleka/currency-converter/util"
 	"github.com/charmbracelet/huh"
 )
 
 type Currency string
 
-var CurrenciesList = map[Currency]string{
+var Currencies = map[Currency]string{
 	"USD": "United States Dollar",
 	"EUR": "Euro",
 	"EGP": "Egyptian Pound",
 	"GBP": "British Pound",
 }
 
+func CurrenciesList() *[]Currency {
+	orderedSlice := util.MapToOrderedSlice[Currency](Currencies)
+	return &orderedSlice
+}
+
 func HuhOptions() *[]huh.Option[string] {
 	cOptions := []huh.Option[string]{}
-	for k, v := range CurrenciesList {
-		label := fmt.Sprintf("%s (%s)", k, v)
-		cOptions = append(cOptions, huh.NewOption(label, string(k)))
+	cl := *CurrenciesList()
+
+	for _, ckey := range cl {
+		c, ok := Currencies[ckey]
+		if !ok {
+			continue
+		}
+		label := fmt.Sprintf("%s (%s)", ckey, c)
+		cOptions = append(cOptions, huh.NewOption(label, string(ckey)))
 	}
 	return &cOptions
 }
 
 func IsSupportedCurrency(c Currency) bool {
-	if _, ok := CurrenciesList[c]; ok {
+	if _, ok := Currencies[c]; ok {
 		return true
 	}
 	return false
